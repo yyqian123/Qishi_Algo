369cb1c9fededebef7404fc5d2798b5009747ebe
yyqian123 <47199842+yyqian123@users.noreply.github.com>
2019-06-15 00:31:17 -0400

    Update Homework_2.py

--- Homework_2.py
+++ Homework_2.py
@@ -31,24 +31,29 @@ class Solution:
 #74. Search a 2D Matrix
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if len(matrix)==0:
+        if matrix is None or len(matrix)==0:
             return False
-        else:
-            n,m=len(matrix),len(matrix[0])
-            if n*m<3:
-                return target==matrix[0][0] or target==matrix[1][0] or target==matrix[0][1]
+        n, m = len(matrix), len(matrix[0])
+        if m==0:
+            return False
+        start, end = 0, n * m - 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            x, y = mid // m, mid % m
+            if matrix[x][y] < target:
+                start = mid
             else:
-                start,end=0,n*m-1
-                while start+1<end:
-                    mid=start+(end-start)//2
-                    x,y=mid//m,mid%m
-                    if matrix[x][y] == target:
-                        return True
-                    elif matrix[x][y] > target:
-                        end = mid
-                    else:
-                        start = mid
-                return False
+                end = mid
+        x, y = start // m, start % m
+        if matrix[x][y] == target:
+            return True
+        
+        x, y = end // m, end % m
+        if matrix[x][y] == target:
+            return True
+        
+        return False
+        
       
 # 34. Find First and Last Position of Element in Sorted Array    
 class Solution:
